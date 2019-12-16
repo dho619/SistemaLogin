7eeb0bac4df136af132df7be81f9f99efb772477
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2019-12-16 12:57:58 -0300

    backend planos ok

--- backend/resolvers/Mutation/Plano.js
+++ backend/resolvers/Mutation/Plano.js
@@ -1,4 +1,4 @@
-const { planos, quest_Planos, questionarios,proximoId7, proximoId8 } = require('../../data/db')
+const { planos, quest_Planos,proximoId7, proximoId8 } = require('../../data/db')
 const { plano: obterPlano } = require('../Query/Plano')
 const { questionario: obterQuestionario } = require('../Query/Questionario')
 
@@ -42,7 +42,7 @@ module.exports = {
 
         if (!id) throw new Error('Filtro nescessario!')
         
-        plano = obterPlano(_, filtro, ctx)
+        plano = obterPlano(_, {filtro}, ctx)
         ctx.validarUsuarioFiltro({id: plano.usuario_id})
         
         let i = planos.findIndex(p => p.id === id)
@@ -50,25 +50,40 @@ module.exports = {
             ...plano,
             ativo: false,
         }
-        plano.splice(i, 1, planoAlterado)
+        planos.splice(i, 1, planoAlterado)
 
         return planoAlterado
     },
 
     alterarPlano(_, { filtro, dados }, ctx) {
-        const { id } = filtro
-
-        if (!id) throw new Error('Filtro nescessario!')
+        if (!filtro) throw new Error('Filtro nescessario!')
         
-        plano = obterPlano(_, filtro, ctx)
+        plano = obterPlano(_, {filtro}, ctx)
         ctx.validarUsuarioFiltro({id: plano.usuario_id})
 
-        const planoAlterado = {
-            ...plano,
-            ...dados
+        const {questionarios} = dados
+        if (!questionarios) throw new Error('Questionarios nescessarios!')
+
+        //eliminando ligaçoes desse plano
+        for(let i in quest_Planos){
+            if(quest_Planos[i].plano_id === plano.id){
+                quest_Planos.splice(i, 1, {})
+            }
         }
 
-        plano.splice(i, 1, planoAlterado)
-        return planoAlterado
+        //adicionando questionarios
+        for(filtro of questionarios){
+            let questionario = obterQuestionario(_, {filtro}, ctx)
+            if(questionario){
+                let novoQuest = {
+                    id: proximoId8(),
+                    plano_id: plano.id,
+                    questionario_id: questionario.id
+                }
+                quest_Planos.push(novoQuest)
+            }
+        }
+
+        return plano
     }
 }
